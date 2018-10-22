219bb3ce4657f2d89a9037da17bde4af7ac8d56b
LePutatu <nikolastkanev@gmail.com>
2018-10-22 19:12:44 +0300

    adding router comments

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const Authentication = require('./controllers/authentication');
 
+//setting up easy to read controllers for the listeners
 module.exports = function (app) {
   app.post('/signup', Authentication.signup)
 };
